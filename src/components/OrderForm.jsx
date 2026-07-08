10d92ea09f6ac521ecf741ba2ca7ef164d10b35a
Surya-58 <mrsuryas445@gmail.com>
2026-07-08 16:05:22 +0530

    Implemented OrderForm with customer, product and quantity inputs

---
+++ src/components/OrderForm.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+
+const OrderForm = ({
+  users,
+  products,
+  customer,
+  setCustomer,
+  product,
+  setProduct,
+  quantity,
+  setQuantity,
+  price,
+  setPrice,
+}) => {
+  return (
+    <div>
+      <label className="label">Customer</label>
+      <br />
+
+      <select
+        className="input"
+        value={customer}
+        onChange={(e) => setCustomer(e.target.value)}
+      >
+        <option value="">Select Customer</option>
+
+        {users.map((user) => (
+          <option key={user.id} value={user.name}>
+            {user.name}</option>
+        ))}
+      </select>
+
+      <br />
+
+      <label className="label">Products</label>
+      <br />
+
+      <select 
+      className="input"
+      value={product}
+      onChange={(e)=>setProduct(e.target.value)}
+      >
+        <option value="">Select Product</option>
+        {products.map((product) => (
+          <option key={product.id} value={product.name} >
+            {product.name}</option>
+        ))}
+      </select>
+      <br />
+
+      <label className="label">Quantity</label>
+      <br />
+
+      <input 
+      type="number" 
+      className="input" 
+      value={quantity}
+      onChange={(e) => setQuantity(e.target.value)}
+      placeholder="Enter Quantity"
+      />
+      <br />
+    </div>
+  );
+};
+
+export default OrderForm;
--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,8 +1,69 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {
+  getOrders,
+  addOrder,
+  updateOrder,
+  deleteOrder,
+  getProducts,
+  getUsers,
+} from "../services/api"
+import OrderForm from '../components/OrderForm'
 
 const Orders = () => {
+
+  const [customer ,setCustomer] = useState("")
+  const [product, setProduct] = useState("")
+  const [quantity, setQuantity] = useState("")
+  const [price, setPrice] = useState("")
+  const [status, setStatus] = useState("Pending")
+
+  const [orders, setOrders] = useState([])
+  const [products, setProducts] = useState([])
+  const [users, setUsers] = useState([])
+
+  const [message, setMessage] = useState("")
+  const [editId, setEditId] = useState(null)
+
+  const handleGetOrders = async() => {
+    const data  = await getOrders()
+    console.log("Orders :",data );
+    setOrders(data)
+  }
+
+  const handleGetProducts = async() => {
+    const data = await getProducts()
+    console.log("Products :",data );
+    setProducts(data)
+  }
+
+  const handleGetUsers = async() => {
+    const data = await getUsers()
+    console.log("Users :",data );
+    setUsers(data)
+  }
+
+  useEffect(()=>{
+    handleGetOrders()
+    handleGetProducts()
+    handleGetUsers()
+  }, [])
+
   return (
-    <div>Orders Page</div>
+    <div>
+      <OrderForm 
+      users={users}
+      products={products}
+      customer={customer}
+      setCustomer={setCustomer}
+      product={product}
+      setProduct={setProduct}
+
+      quantity={quantity}
+      setQuantity={setQuantity}
+      price={price}
+      setPrice={setPrice}
+       />
+    </div>
   )
 }
 
