7e9433f8f6d686670c37e870c57ccf0a306c1a66
Surya-58 <mrsuryas445@gmail.com>
2026-07-10 11:20:24 +0530

    Optimized dashboard data loading with Promise.all

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,45 @@
 import React from "react";
 import DashboardCard from "../components/DashboardCard";
 import "../styles/Home.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { getCategories, getOrders, getProducts, getUsers } from "../services/api";
 
 const Home = () => {
-  const [stats] = useState({
-    products : 120,
-    orders : 56,
-    users : 30,
-    categories :8
-  })
+  const [products, setProducts] = useState([])
+  const [users, setUsers] = useState([])
+  const [orders, setOrders] = useState([])
+  const [categories, setCategories] = useState([])
+
+  useEffect(()=>{
+    const loadDashboard = async() => {
+      const [
+        productsData,
+        usersData,
+        ordersData,
+        categoriesData
+      ] = await Promise.all([
+        getProducts(),
+        getUsers(),
+        getOrders(),
+        getCategories(),
+      ])
+      setProducts(productsData)
+      setUsers(usersData)
+      setOrders(ordersData)
+      setCategories(categoriesData)
+    }
+    loadDashboard()
+
+  },[])
 
   return (
     <div>
       <h1>Dashboard</h1>
       <div className="dashboard-grid">
-        <DashboardCard title="Products" value={stats.products} />
-        <DashboardCard title="Orders" value={stats.orders} />
-        <DashboardCard title="Users" value={stats.users} />
-        <DashboardCard title="Categories" value={stats.categories} />
+        <DashboardCard title="Products" value={products.length} />
+        <DashboardCard title="Orders" value={orders.length} />
+        <DashboardCard title="Users" value={users.length} />
+        <DashboardCard title="Categories" value={categories.length} />
       </div>
     </div>
   );
